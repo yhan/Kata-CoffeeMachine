33fce34a7d2e35ac317792af42dc78468515b977
han <yhan@la-francaise.com>
2018-10-19 08:50:36 +0200

    fix  the date of today for easing unit test

--- CoffeeMachine/LogicShould.cs
+++ CoffeeMachine/LogicShould.cs
@@ -18,18 +18,18 @@
         public void Setup()
         {
             this._todayProvider = Substitute.For<IProvideToday>();
-            this._todayProvider.GetToday().Returns(DateTime.Today);
+            this._todayProvider.GetToday().Returns(new DateTime(2018, 10, 19));
             _logic = new Logic(this._todayProvider);
         }
 
 
-        [TestCase(ProductKind.Coffee,"C:0:0")]
-        [TestCase(ProductKind.Chocolate,  "H:0:0")]
+        [TestCase(ProductKind.Coffee, "C:0:0")]
+        [TestCase(ProductKind.Chocolate, "H:0:0")]
         [TestCase(ProductKind.OrangeJuice, "O:0:0")]
         [TestCase(ProductKind.Tea, "T:0:0")]
         public void Return_1_drink_0_sugar_0_stick_When_customer_ask_for_a_specific_drink(ProductKind productKind, string expected)
         {
-            var actual = _logic.Translate(new Order {ProductKind = productKind, Sugar = 0});
+            var actual = _logic.Translate(new Order { ProductKind = productKind, Sugar = 0 });
             Check.That(actual).IsEqualTo(expected);
         }
 
@@ -39,7 +39,7 @@
         [TestCase(ProductKind.Tea, "T:1:1")]
         public void Return_1_drink_1_sugar_1_stick_When_customer_ask_for_specific_drink(ProductKind productKind, string expected)
         {
-            var actual = _logic.Translate(new Order {ProductKind = productKind, Sugar = 1});
+            var actual = _logic.Translate(new Order { ProductKind = productKind, Sugar = 1 });
             Check.That(actual).IsEqualTo(expected);
         }
 
@@ -49,15 +49,15 @@
         [TestCase(ProductKind.Tea, "T:2:1")]
         public void Return_1_drink_2_sugar_1_stick_When_customer_ask_for_a_specific_drink(ProductKind productKind, string expected)
         {
-            var actual = _logic.Translate(new Order {ProductKind = productKind, Sugar = 2});
+            var actual = _logic.Translate(new Order { ProductKind = productKind, Sugar = 2 });
             Check.That(actual).IsEqualTo(expected);
         }
-        
+
         [Test]
         public void Return_1_coffee_0_sugar_0_stick_When_customer_ask_for_coffee_with_60_centimes()
         {
-            
-            var actual = _logic.Translate(new Order {ProductKind = ProductKind.Coffee, Sugar = 0, Money = 0.6});
+
+            var actual = _logic.Translate(new Order { ProductKind = ProductKind.Coffee, Sugar = 0, Money = 0.6 });
             Check.That(actual).IsEqualTo("C:0:0");
         }
         /// <summary>
@@ -82,7 +82,7 @@
         [TestCase(ProductKind.Tea, 0.3)]
         public void Return_message_When_10_cents_is_not_enough_money_for_a_coffee(ProductKind productKind, double missingMoney)
         {
-            var actual = _logic.Translate(new Order {ProductKind = productKind, Sugar = 0, Money = 0.1});
+            var actual = _logic.Translate(new Order { ProductKind = productKind, Sugar = 0, Money = 0.1 });
             Check.That(actual).IsEqualTo($"M:Not enough money, missing {missingMoney}");
         }
 
@@ -105,7 +105,7 @@
 
             string report = this._logic.Report();
 
-            Check.That(report).IsEqualTo("(17/10/2018)| 1.2 euro, Coffee: 1, Orange: 1");
+            Check.That(report).IsEqualTo("(19/10/2018)| 1.2 euro, Coffee: 1, Orange: 1");
         }
 
         [Test]
@@ -128,7 +128,7 @@
             this._logic.Translate(new Order(ProductKind.Coffee, extraHot: false, money: 0.6, sugar: 1));
 
             _todayProvider.GetToday().Returns(DateTime.Today.AddDays(1));
-            
+
             this._logic.Translate(new Order(ProductKind.Coffee, extraHot: false, money: 0.6, sugar: 1));
             this._logic.Translate(new Order(ProductKind.OrangeJuice, extraHot: false, money: 0.6, sugar: 1));
 
